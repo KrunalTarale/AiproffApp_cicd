ebeeada7211c74b8061675e7e8c25eda21d9ae77
Krunal <krunaltarale529@gmail.com>
2023-10-06 12:48:50 +0530

    add cards

--- frontend/src/components/Buy/Buy.jsx
+++ frontend/src/components/Buy/Buy.jsx
@@ -8,9 +8,9 @@ const Buy = () => {
     <div>
       <Navbar />
 
+      <div className="category">
+      <h1 className="cat_head">Product Category</h1>
       <div className="allcards">
-
-
         <div className="_cards">
           <div className="_card-image"></div>
           <div className="_category"> Illustration </div>
@@ -47,10 +47,49 @@ const Buy = () => {
             </div>
           </div>
         </div>
-
-
       </div>
-
+      </div>
+      <div className="category">
+      <h1 className="cat_head">Product Category</h1>
+      <div className="allcards">
+        <div className="_cards">
+          <div className="_card-image"></div>
+          <div className="_category"> Illustration </div>
+          <div className="_heading">
+            {" "}
+            A heading that must span over two lines
+            <div className="_author">
+              {" "}
+              By <span className="_name">Abi</span> 4 days ago
+            </div>
+          </div>
+        </div>
+        <div className="_cards">
+          <div className="_card-image"></div>
+          <div className="_category"> Illustration </div>
+          <div className="_heading">
+            {" "}
+            A heading that must span over two lines
+            <div className="_author">
+              {" "}
+              By <span className="_name">Abi</span> 4 days ago
+            </div>
+          </div>
+        </div>
+        <div className="_cards">
+          <div className="_card-image"></div>
+          <div className="_category"> Illustration </div>
+          <div className="_heading">
+            {" "}
+            A heading that must span over two lines
+            <div className="_author">
+              {" "}
+              By <span className="_name">Abi</span> 4 days ago
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
       <Footer />
     </div>
   );
